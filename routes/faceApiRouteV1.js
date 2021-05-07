dcd18196f01a67447e78223071e0ec1827878157
Prabhav Jani <31888813+progressivePRV@users.noreply.github.com>
2021-05-07 03:24:41 -0400

    changed environment vaaraible

--- routes/faceApiRouteV1.js
+++ routes/faceApiRouteV1.js
@@ -6,7 +6,7 @@ const axios = require('axios');
 // const swaggerJsDoc = require("swagger-jsdoc");
 
 
-const key = "8e7a4980790d4112b69a007bab0349a7";
+const key = preocess.env.Azure_Api_k;
 const endpoint = "https://face-api-project-for-si.cognitiveservices.azure.com/";
 
 
